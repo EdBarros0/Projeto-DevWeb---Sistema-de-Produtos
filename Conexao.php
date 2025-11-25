54ab3bd275e69999dcc71c27e0da5017800be0e7
Leo-dk574 <leonardoamaralvalladao@gmail.com>
2025-11-25 20:14:05 -0300

    Conexao.php

---
+++ Conexao.php
@@ -0,0 +1,15 @@
+<?php
+	$host = "localhost:3306";
+	$user="root";
+	$pass ="";
+	$dbname = "SisWeb";
+	//Conexao
+	 $con = mysqli_connect($host, $user, $pass, $dbname);
+          // TESTAR CONEXAO
+         if(!$con) {
+             echo "<p>ERRO: (" . mysqli_connect_errno($con) . ") " . mysqli_connect_error($con) . "</p>";
+             exit;
+          } else {
+            echo "<p>Conexão realizada com sucesso!</p>";
+         }
+?>
