e6a554077dc83f072618315df491681ac225e33b
Leo-dk574 <leonardoamaralvalladao@gmail.com>
2025-11-25 21:09:12 -0300

    insereProduto.php

--- insereProduto.php
+++ insereProduto.php
@@ -5,12 +5,23 @@
     $validade =$_POST['Validade'];
     $valor_c =$_POST['Valor_Custo'];
     $valor_v = $_POST['Valor_Venda'];
+    $id_f = $_POST['Id_f'];
 
     $sql = "INSERT INTO Produto (Nome, Validade, Valor_Custo, Valor_Venda) VALUES ('$nome','$validade','$valor_c','$valor_v')";
 
     if(mysqli_query($con, $sql)){
+
+        $idproduto = mysqli_insert_id($con);
                     
-        echo json_encode(["mensagem" => "Produto cadastrado com sucesso!!!!"]);
+        $sql2 = "INSERT INTO Func_Prod (Id_f, Id_p) VALUES ('$id_f','$idproduto')";
+
+        if(mysqli_query($con,$sql2)){
+
+            echo json_encode(["mensagem" => "Produto cadastrado e relação criada!"]);
+    } else {
+        echo "Produto inserido, mas falhou ao criar relação!";
+        }
+       
             
     }
     else{
