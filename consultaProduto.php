861674fb462aa8c3f21fdbd769a6c557c95265c3
Leo-dk574 <leonardoamaralvalladao@gmail.com>
2025-11-27 00:14:46 -0300

    consultaProduto.php

--- consultaProduto.php
+++ consultaProduto.php
@@ -1,10 +1,25 @@
 <?php 
-	    require_once 'conexao.php';
+	    session_start();
+        require_once 'conexao.php';
+
+        $id_f = $_SESSION['Id_f'] ?? null;
+   
+        if ($id_f === null) {
+            echo json_encode(["Erro" => "Usuário não logado."]);
+        exit;
+        }
 
 		$nome = $_POST['Nome'];
         
-		$sql = "SELECT Nome, Validade, Valor_Custo, Valor_Venda FROM Produto WHERE Nome LIKE '%$nome%'";
+		if (empty($nome)) {
+        echo json_encode(["Erro" => "Digite um nome para pesquisar."]);
+        exit;
+        }
 
+
+		$sql = "SELECT Id_p, Nome, Valor_Custo, Valor_Venda, Quantidade FROM Produto WHERE Nome LIKE '%$nome%'";
+
+		
 		$result = mysqli_query($con, $sql);
         $produtos = [];
 
@@ -18,4 +33,5 @@
             echo json_encode(["Erro" => "Produto não encontrado!"]);
         }
 
+        mysqli_close($con);
 ?>
