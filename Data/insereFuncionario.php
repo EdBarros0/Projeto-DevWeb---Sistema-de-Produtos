e4e2c3a4920e1eb3a8570a0ef4dffe5ea31ebcbd
Leo-dk574 <leonardoamaralvalladao@gmail.com>
2025-11-27 00:10:31 -0300

    insereFuncionario.php

--- Data/insereFuncionario.php
+++ Data/insereFuncionario.php
@@ -5,20 +5,21 @@
     $email =$_POST['Email'];
     $senha =$_POST['Senha'];
     $telefone = $_POST['Telefone'];
+    $cargo = $_POST['Cargo'];
 
-    if (empty($nome) || empty($email) || empty($senha) || empty($telefone)) {
+    if (empty($nome) || empty($email) || empty($senha) || empty($telefone) || empty($cargo)) {
     echo json_encode(["erro" => "Preencha todos os campos!"]);
     exit;
 }
-    $sql = "INSERT INTO Funcionario (Nome, Email, Senha, Telefone) VALUES ('$nome','$email','$senha','$telefone')";
+    $sql = "INSERT INTO Funcionario (Nome, Email, Senha, Telefone, Cargo) VALUES ('$nome','$email','$senha','$telefone','$cargo')";
 
     if(mysqli_query($con, $sql)){
                     
-        echo json_encode(["mensagem" => "Funcionario cadastrado com sucesso!!!!"]);
+        echo json_encode(["mensagem" => "Funcionário cadastrado com sucesso!!!!"]);
             
     }
     else{
-        echo json_encode(["Erro" => "Não foi possivel cadastrar o Funcionario!"]);
+        echo json_encode(["Erro" => "Não foi possível cadastrar o Funcionário!"]);
     }
                 
     mysqli_close($con);
