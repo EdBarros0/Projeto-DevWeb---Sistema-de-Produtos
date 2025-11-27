4e76eb8d718d281855e82d18ff9a1dba0a345c43
Leo-dk574 <leonardoamaralvalladao@gmail.com>
2025-11-27 00:12:37 -0300

    editaProduto.php

--- Data/editaProduto.php
+++ Data/editaProduto.php
@@ -1,39 +1,60 @@
 <?php
+    session_start();
+    require_once 'conexao.php';
 
-require_once 'conexao.php';
+    $id_f = $_SESSION['Id_f'] ?? null;
 
-$idproduto = $_POST['Id_p'];
-$idfuncionario = $_POST['Id_f'];
-$nome = $_POST['Nome'];
-$validade =$_POST['Validade'];
-$valor_c =$_POST['Valor_Custo'];
-$valor_v = $_POST['Valor_Venda'];
-
-
-if(empty($idproduto)){
-    echo json_encode(["Erro" => "Digite o id do Produto."]);
+    if ($id_f === null) {
+        echo json_encode(["Erro" => "Usuário não logado."]);
     exit;
-}
+    }
 
-$idfuncionario = intval($idfuncionario);
-$idproduto = intval($idproduto);
+    $id_p       = $_POST['Id_p'] ?? null;
+    $nome       = $_POST['Nome'] ?? null;
+    $valor_c    = $_POST['Valor_Custo'] ?? null;
+    $valor_v    = $_POST['Valor_Venda'] ?? null;
+    $quantidade = $_POST['Quantidade'] ?? null;
 
-$sql = "UPDATE Produto  
-SET Nome = '$nome', 
-Validade = '$validade',
-Valor_Custo = '$valor_c',
-Valor_Venda = '$valor_v'
-WHERE Id_p = '$idproduto'";
+    if (empty($id_p)) {
+        echo json_encode(["Erro" => "Id do produto não informado!"]);
+    exit;
+    }
 
-$sql2 = "UPDATE Func_Prod
-SET Id_f = '$idfuncionario'
-WHERE Id_p = '$idproduto'"; 
+    if (empty($nome)) {
+        echo json_encode(["Erro" => "O produto precisa ter um nome!"]);
+    exit;
+    }
 
-if(mysqli_query($con,$sql) && mysqli_query($con,$sql2)){
-    echo json_encode(["Sucesso" => "Produto atualizado com sucesso!"]);
-} else {
-    echo json_encode(["Erro" => "Erro na Atualização!"]);
-}
+    if (empty($valor_c)) {
+        echo json_encode(["Erro" => "O produto precisa ter um valor de custo!"]);
+    exit;
+    }
+
+    if (empty($valor_v)) {
+        echo json_encode(["Erro" => "O produto precisa ter um valor de venda!"]);
+    exit;
+    }
+
+    if (empty($quantidade)) {
+        echo json_encode(["Erro" => "O produto precisa ter uma quantidade!"]);
+    exit;
+    }
+
+    $sql = "UPDATE Produto
+    SET Nome = '$nome',
+    Valor_Custo = '$valor_c',
+    Valor_Venda = '$valor_v',
+    Quantidade = '$quantidade'
+    WHERE Id_p = '$id_p'";
+
+    $sql2 = "UPDATE Func_Prod
+    SET Id_f = '$id_f'
+    WHERE Id_p = '$id_p'";
+
+    if (mysqli_query($con, $sql) && mysqli_query($con, $sql2)) {
+        echo json_encode(["Sucesso" => "Produto atualizado com sucesso!"]);
+    } else {
+        echo json_encode(["Erro" => "Erro na atualização!"]);
+    }
 
 mysqli_close($con);
-?>
