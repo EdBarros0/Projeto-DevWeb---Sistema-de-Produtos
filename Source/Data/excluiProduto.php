0c48b34a70916fc00c7c510525e2f6bd56e20cc3
Leo-dk574 <leonardoamaralvalladao@gmail.com>
2025-11-27 20:45:32 -0300

    excluiProduto.php

--- Source/Data/excluiProduto.php
+++ Source/Data/excluiProduto.php
@@ -9,18 +9,16 @@
         exit;
     }
     
-    $id_p = $_POST['Id_p'] ?? null;
+    $nome = $_POST['Nome'] ?? null;
 
-    if(empty($id_p)){
-        echo json_encode(["Erro" => "Diga o id do produto."]);
+    if(empty($nome)){
+        echo json_encode(["Erro" => "Diga o nome do produto."]);
     exit;
     }
-    
-    $sql = " DELETE FROM Func_Prod WHERE Id_p = '$id_p'";
 
-    $sql2 = "DELETE FROM Produto WHERE Id_p = '$id_p'";
+    $sql = "DELETE FROM Produto WHERE Nome = '$nome'";
 
-    if(mysqli_query($con,$sql) && mysqli_query($con,$sql2) ){
+    if(mysqli_query($con,$sql)){
         echo json_encode(["Sucesso" => "Produto excluído com sucesso!"]);
     } else {
         echo json_encode(["Erro" => "Erro na Exclusão!"]);
